fb3f031b3fdc887e45724c2c68e7fafa309d4df8
Łukasz Deja <lukideja41@gmail.com>
2026-05-01 15:00:42 +0200

    Utworzone repo serwera

---
+++ src/test/java/com/KryptoChat/serwer/SerwerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KryptoChat.serwer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
