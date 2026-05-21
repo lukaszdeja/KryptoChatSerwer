5802a49ace572e1f4d9afc2d35cfdd0fa12e6e98
Łukasz Deja <lukideja41@gmail.com>
2026-05-21 11:29:20 +0200

    Obsluga jwt w zaczytywaniu wiadomosci

--- src/main/java/com/KryptoChat/serwer/controllers/MessageController.java
+++ src/main/java/com/KryptoChat/serwer/controllers/MessageController.java
@@ -1,5 +1,8 @@
 package com.KryptoChat.serwer.controllers;
 
+import com.KryptoChat.serwer.entities.User;
+import com.KryptoChat.serwer.services.JWTService;
+import com.KryptoChat.serwer.services.UserService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import com.KryptoChat.serwer.services.ChatService;
@@ -11,14 +14,32 @@ import java.util.ArrayList;
 @RequestMapping("api/messages")
 public class MessageController {
     private final ChatService chatService;
+    private final UserService userService;
 
-    public MessageController(ChatService chatService) {
+    public MessageController(UserService userService, ChatService chatService) {
+        this.userService = userService;
         this.chatService = chatService;
     }
 
-    @GetMapping("{id}")
-    public ResponseEntity<MessageList> loadMessages(@PathVariable Long id) {
-        MessageList messages = chatService.loadMessages(id);
+    @GetMapping("/")
+    public ResponseEntity<MessageList> loadMessages(@RequestHeader("Authorization")  String header) {
+        if (header == null || !header.startsWith("Bearer ")) {
+            return ResponseEntity.status(401).build();
+        }
+
+        String token = header.substring(7);
+
+        JWTService jwtService = new JWTService();
+
+        if (!jwtService.isTokenValid(token)) {
+            return ResponseEntity.status(401).build();
+        }
+        Long userId = jwtService.extractUserId(token);
+
+        User user = userService.authentification(userId);
+
+        Long groupId = user.getGroup().getId();
+        MessageList messages = chatService.loadMessages(groupId);
         return ResponseEntity.ok(messages);
     }
 }
