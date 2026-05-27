a91775789868adc633a483bf7d9f86df55924e75
Nacia <natiparszywka@gmail.com>
2026-05-27 19:14:17 +0200

    Praca nad szyfrowaniem

--- src/main/java/com/KryptoChat/serwer/entities/GroupKey.java
+++ src/main/java/com/KryptoChat/serwer/entities/GroupKey.java
@@ -33,8 +33,16 @@ public class GroupKey {
         return groupId;
     }
 
+    public void setGroupId(Long groupId) {
+        this.groupId = groupId;
+    }
+
     public Long getUserId() {
         return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
     }
 
     public String getEncryptedGroupKey() {
--- src/main/java/com/KryptoChat/serwer/services/GroupService.java
+++ src/main/java/com/KryptoChat/serwer/services/GroupService.java
@@ -4,31 +4,53 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 import com.KryptoChat.serwer.repositories.*;
 import com.KryptoChat.serwer.entities.*;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.util.Base64;
+import java.util.List;
+import java.util.UUID;
+
+import com.KryptoChat.serwer.security.RSAEncryptionUtil;
+
+import static com.KryptoChat.serwer.security.RSAEncryptionUtil.encryptRSA;
 
 @Service
 public class GroupService {
 
     private final GroupRepository groupRepository;
     private final UserRepository userRepository;
+    private final GroupKeyRepository groupKeyRepository;
 
-    public GroupService(GroupRepository groupRepository, UserRepository userRepository) {
+    public GroupService(GroupRepository groupRepository, UserRepository userRepository, GroupKeyRepository groupkey) {
         this.groupRepository = groupRepository;
         this.userRepository = userRepository;
+        this.groupKeyRepository = groupkey;
     }
 
     @Transactional
-    public Long createGroup(String name, User user) {
+    public Long createGroup(String groupName, User creator) {
 
-        String code;
-        do {
-            code = GroupCodeGenerator.generateCode();
-        } while (groupRepository.findByKod(code).isPresent());
+        Group group = new Group();
+        group.setGroupName(groupName);
+        group.setKod(UUID.randomUUID().toString().substring(0, 8));
 
-        Group group = new Group(name, code);
         groupRepository.save(group);
 
-        user.setGroup(group);
-        userRepository.save(user);
+        String aesKey = generateAESKey();
+
+        List<User> users = List.of(creator);
+
+        for (User user : users) {
+
+            String encryptedKey = encryptRSA(aesKey, user.getPublicKey());
+
+            GroupKey gk = new GroupKey(group.getId(), user.getId(), encryptedKey);
+
+            groupKeyRepository.save(gk);
+        }
+
+        creator.setGroup(group);
+        userRepository.save(creator);
 
         return group.getId();
     }
@@ -37,11 +59,37 @@ public class GroupService {
     public Long joinGroup(String code, User user) {
 
         Group group = groupRepository.findByKod(code)
-                .orElseThrow(() -> new RuntimeException("Nie znaleziono grupy"));
+                .orElseThrow(() -> new RuntimeException("Brak grupy"));
 
         user.setGroup(group);
         userRepository.save(user);
 
+        String aesKey = getAESKeyForGroup(group.getId());
+
+        String encrypted = encryptRSA(aesKey, user.getPublicKey());
+
+        GroupKey gk = new GroupKey(group.getId(), user.getId(), encrypted);
+
+        groupKeyRepository.save(gk);
+
         return group.getId();
+    }
+
+    public String generateAESKey() {
+        try {
+            KeyGenerator generator = KeyGenerator.getInstance("AES");
+            generator.init(256);
+
+            SecretKey key = generator.generateKey();
+
+            return Base64.getEncoder().encodeToString(key.getEncoded());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String getAESKeyForGroup(Long groupId) {
+        // tu cos ma byc
+        throw new RuntimeException("TODO: store group AES key securely");
     }
 }
